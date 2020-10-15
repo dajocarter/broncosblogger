43a1c1240070b371fadffc7e01669595c935f5d6
David Carter <dajocarter@gmail.com>
2020-10-15 11:46:15 -0700

    create a default layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = {
   dir: {
     input: ".", // specify input directory
+    data: "_data", // specify data config directory
+    includes: "_includes", // specify layouts directory
     output: "_site" // specify output directory
   }
 }
