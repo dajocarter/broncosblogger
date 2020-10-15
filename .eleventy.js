3500ea0e7cf5160dd580d1995217ea396a4ff902
David Carter <dajocarter@gmail.com>
2020-10-15 13:30:07 -0700

    install postcss and tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
-module.exports = {
-  dir: {
-    input: ".", // specify input directory
-    data: "_data", // specify data config directory
-    includes: "_includes", // specify layouts directory
-    output: "_site" // specify output directory
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css')
+
+  return {
+      dir: {
+      input: ".", // specify input directory
+      data: "_data", // specify data config directory
+      includes: "_includes", // specify layouts directory
+      output: "_site" // specify output directory
+    }
   }
 }
